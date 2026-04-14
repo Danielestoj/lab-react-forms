02b74a8e308003eac886b6d531d47d0766b67081
Danielestoj <danielestoj@gmail.com>
2026-04-14 17:25:57 +0100

    ejercicio resuelto

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,158 @@
+import { useState } from "react";
+
+function AddStudent({ handleAddStudent }) {
+  // ESTADOS DEL FORMULARIO
+  const [fullName, setFullName] = useState("");
+  const [image, setProfileImage] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [program, setProgram] = useState("-- None --");
+  const [graduationYear, setGraduationYear] = useState(0);
+  const [graduated, setGraduated] = useState(false);
+
+  // FUNCIÓN QUE SE EJECUTA AL ENVIAR EL FORMULARIO
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    // CREACIÓN DEL OBJETO CON LOS DATOS DEL NUEVO ESTUDIANTE
+    const newStudent = { 
+      fullName: fullName,
+      email: email,
+      phone: phone,
+      program: program,
+      image: image,
+      graduationYear: Number(graduationYear),
+      graduated: graduated,
+    };
+
+    // ENVÍA EL NUEVO ESTUDIANTE AL COMPONENTE PADRE
+    handleAddStudent(newStudent);
+
+    // LIMPIA EL FORMULARIO DESPUÉS DE ENVIARLO
+    setFullName("");
+    setProfileImage("");
+    setPhone("");
+    setEmail("");
+    setProgram("-- None --");
+    setGraduationYear(0);
+    setGraduated(false);
+  }
+
+  return (
+    <div className="App pt-20">
+
+      {/* FORMULARIO */}
+      <form onSubmit={handleFormSubmit}>
+        <span>Add a Student</span>
+
+        {/* DATOS BÁSICOS DEL ESTUDIANTE */}
+        <div>
+          <label>
+            Full Name
+            <input
+              name="fullName"
+              type="text"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={(event) => {
+                setFullName(event.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Profile Image
+            <input
+              name="image"
+              type="url"
+              placeholder="Profile Image"
+              value={image}
+              onChange={(event) => {
+                setProfileImage(event.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Phone
+            <input
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              value={phone}
+              onChange={(event) => {
+                setPhone(event.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Email
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+          </label>
+        </div>
+
+        {/* INFORMACIÓN ACADÉMICA */}
+        <div>
+          <label>
+            Program
+            <select
+              name="program"
+              value={program}
+              onChange={(event) => {
+                setProgram(event.target.value);
+              }}
+            >
+              <option value="">-- None --</option>
+              <option value="Web Dev">Web Dev</option>
+              <option value="UXUI">UXUI</option>
+              <option value="Data">Data</option>
+            </select>
+          </label>
+
+          <label>
+            Graduation Year
+            <input
+              name="graduationYear"
+              type="number"
+              placeholder="Graduation Year"
+              minLength={4}
+              maxLength={4}
+              min={2023}
+              max={2030}
+              value={graduationYear}
+              onChange={(event) => {
+                setGraduationYear(event.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Graduated
+            <input
+              name="graduated"
+              type="checkbox"
+              value={graduated}
+              onChange={(event) => {
+                setGraduated(event.target.checked);
+              }}
+            />
+          </label>
+
+          <button type="submit">Add Student</button>
+        </div>
+      </form>
+      {/* FIN DEL FORMULARIO */}
+    </div>
+  );
+}
+
+export default AddStudent;
